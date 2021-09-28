21caa08aae35c711a13bbb5a5b090e5d604bc821
liebercanis <mgold@unm.edu>
2021-09-28 11:55:43 -0600

    tools

---
+++ eventAna.C
@@ -0,0 +1,529 @@
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include "BaconAnalysis.hh"
+// time is in microseconds
+using namespace TMath;
+enum { NSETS = 6};
+
+TH1D* hWave[NSETS];
+
+double peakInRange(TH1D* h, double xmin, double xmax){
+  int ilow = h->FindBin(xmin);
+  int ihigh = h->FindBin(xmax);
+  double val=0;
+  double xpeak=0;
+  for(int i=ilow; i<= ihigh; ++i) if(h->GetBinContent(i)>val) {
+    val = h->GetBinContent(i);
+    xpeak = h->GetBinLowEdge(i);
+  }
+  return xpeak;
+}
+
+void weightedMean(  std::vector<double> val, std::vector<double> err, double &mean, double& emean) 
+{
+
+  mean =0;
+  double w =0;
+  for(unsigned i=0; i< val.size(); ++i ) {
+    double wi = 1./pow(err[i] ,2.);
+    mean += wi*val[i];
+    w += wi;
+  }
+  mean /= w;
+  emean = 1./sqrt(w);
+}
+
+void ratioE(double x, double xe, double y, double ye, double& r,double& re){
+  r = x/y;
+  re = r * sqrt( pow(xe/x,2.)+pow(ye/y,2.) );
+}
+
+void getStats(std::vector<double> v, double& mean, double& stdev)
+{
+
+  double sum = std::accumulate(v.begin(), v.end(), 0.0);
+  mean = sum / v.size();
+
+  std::vector<double> diff(v.size());
+  std::transform(v.begin(), v.end(), diff.begin(),
+      std::bind2nd(std::minus<double>(), mean));
+  double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+  stdev = std::sqrt(sq_sum / v.size());
+}
+
+void eventAna()
+{
+  int pcolor[NSETS];
+  pcolor[5] = kBlack;
+  pcolor[4] = kBlue;
+  pcolor[3] = kYellow - 2;
+  pcolor[2] = kGreen;
+  pcolor[1] = kMagenta + 2;
+  pcolor[0] = kRed;
+
+  double PPM[NSETS]={0,1,2,5,10,10};
+
+  double singleCut[NSETS] = {1.,4.,4.,4.,4.,4.};
+  printf(" single cuts are :\n");
+  for (int iset = 0; iset < NSETS; ++iset)
+    printf(" \t set %i cut %.1f \n", iset, singleCut[iset]);
+
+  TFile *inFile = new TFile("BaconAnalysisHighCut-1000.root", "READONLY");
+  //TFile *inFile = new TFile("BaconAnalysis-646.root", "READONLY");
+  if (!inFile)
+    return;
+  TFile *fout = new TFile("eventAna.root", "RECREATE");
+  //
+  TH2D* hRunTriplet= new TH2D("RunTriplet","run triplet",200,0,200,300,-100,200);
+  //
+  TH1D* hLate[NSETS];
+  TH1D* hLateTime[NSETS];
+  TH1D* hSingleSet[NSETS];
+  TH1D* hTripleSet[NSETS];
+  TString hname;
+  TString htitle;
+  for(int iset=0; iset<NSETS; ++iset) {
+    hname.Form("Late%i-%i-PPM",iset,int(PPM[iset]));
+    htitle.Form("Late set %i, %i PPM",iset,int(PPM[iset]));
+    hLate[iset] = new TH1D(hname,htitle,40,0.,.2);
+    hLate[iset]->SetLineColor(pcolor[iset]);
+    hname.Form("LateTime%i-%i-PPM",iset,int(PPM[iset]));
+    htitle.Form("LateTime set %i, %i PPM",iset,int(PPM[iset]));
+    hLateTime[iset] = new TH1D(hname,htitle,200,0.,10000.);
+    hLateTime[iset]->SetLineColor(pcolor[iset]);
+
+    hname.Form("Singlet%i-%i-PPM",iset,int(PPM[iset]));
+    htitle.Form("Singlet yield set %i, %i PPM",iset,int(PPM[iset]));
+    hSingleSet[iset] = new TH1D(hname,htitle,310,-1.,31.);
+    hSingleSet[iset]->SetLineColor(pcolor[iset]);
+
+    hname.Form("Triplet%i-%i-PPM",iset,int(PPM[iset]));
+    htitle.Form("Triplet yield set %i, %i PPM",iset,int(PPM[iset]));
+    hTripleSet[iset] = new TH1D(hname,htitle,400,-100.,300.);
+    hTripleSet[iset]->SetLineColor(pcolor[iset]);
+
+  }
+
+  Float_t sumVars[SUMVARS];
+  int setTotal[NSETS];
+  int setRuns[NSETS];
+  TTree* tSummary=NULL;
+
+  for(int i=0; i<NSETS; ++i) {
+    hWave[i]=NULL;
+    inFile->GetObject(Form("Wave-%i",i),hWave[i]);
+    if(hWave[i]) {
+      cout << " wave hist " << hWave[i]->GetName();
+      fout->Append(hWave[i]);
+    }
+  }
+
+
+
+  inFile->GetObject("Summary",tSummary);
+  if(tSummary) {
+  //TNtuple *ntSummary = new TNtuple("Summary", " Summary ", "run:set:base:baseend:accept:total:singlet:dublet:triplet:ngood");
+  TString sumNames[SUMVARS];
+  sumNames[0]=  TString("run");
+  sumNames[1]=  TString("set");
+  sumNames[2]=  TString("base");
+  sumNames[3]=  TString("baseend");
+  sumNames[4]=   TString("accept");
+  sumNames[5]=  TString("total");
+  sumNames[6]=  TString("singlet");
+  sumNames[7]=  TString("dublet");
+  sumNames[8]=  TString("triplet");
+  sumNames[9]=  TString("ngood");
+
+
+  for(int iv=0; iv< SUMVARS; ++iv ) tSummary->SetBranchAddress(sumNames[iv],&sumVars[iv]);
+
+  for(int iset=0; iset<NSETS; ++iset ) { 
+    setTotal[iset]=0;
+    setRuns[iset]=0;
+  }
+
+  // ratios 
+
+  if(tSummary) printf(" tSummary has  %lld  entries \n",tSummary->GetEntries());
+  else printf(" no tSummary \n ");
+
+  // all entries and fill the histograms
+  for (Long64_t jent=0;jent<tSummary->GetEntries() ;jent ++) {
+    tSummary->GetEntry(jent);
+    //printf(" tSummary entry %lld \n ",jent);
+    for(int iv=0; iv< SUMVARS; ++iv ) {
+      int jset = int(sumVars[ESET]);
+      int nset = int(sumVars[NGOOD]);
+      setTotal[jset]+=  int(sumVars[NGOOD]);
+      ++setRuns[jset];
+      //printf(" \t %i %s %f \n",iv, sumNames[iv].Data(), sumVars[iv]); 
+    }
+  }
+
+  for(int iset=0; iset<NSETS; ++iset ) printf(" set %i runs %i total events %i \n ", iset, setRuns[iset], setTotal[iset]);
+
+  }
+
+  TTree* tPre=NULL;
+  inFile->GetObject("EvPre",tPre); 
+  enum {NPRE=8};
+  float preVars[NPRE];
+  TString preNames[NPRE];
+  preNames[0]=  TString("run");
+  preNames[1]=  TString("set");
+  preNames[2]=  TString("flag");
+  preNames[3]=  TString("sum");
+  preNames[4]=   TString("singlet");
+  preNames[5]=  TString("triplet");
+  preNames[6]=  TString("late");
+  preNames[7]=  TString("latetime");
+
+
+  for(int iv=0; iv< NPRE; ++iv ) tPre->SetBranchAddress(preNames[iv],&preVars[iv]);
+
+  vector<double> prePass;
+  prePass.resize(NPRE);
+  vector<double> preNum;
+  preNum.resize(NPRE);
+
+  for (Long64_t jent=0;jent<tPre->GetEntries() ;jent ++) {
+    tPre->GetEntry(jent);
+    int iset = preVars[1];
+    int flag = int(preVars[2]);
+    preNum[iset] = preNum[iset]+1.;
+    if(flag==0) prePass[iset] = prePass[iset] + 1.;
+  }
+
+  vector<double> preFrac;
+  preFrac.resize(NPRE);
+
+  for(unsigned iset =0; iset<preFrac.size(); ++iset) {
+    preFrac[iset] = prePass[iset]/preNum[iset];
+  }
+
+  vector<double> vset;
+  for(unsigned iset =0; iset<preFrac.size(); ++iset) vset.push_back(double(iset));
+
+
+  TGraph* gPass = new TGraph(NSETS,&vset[0],&preFrac[0]);
+
+  TCanvas *canpre = new TCanvas("prePass","prePass");
+  canpre->SetGridy();
+  gPass->SetName("prePass");
+  gPass->SetTitle("pass fraction ");
+  gPass->SetMarkerColor(kRed);
+  gPass->SetMarkerStyle(22);
+  gPass->SetMarkerSize(1.4);
+  gPass->GetXaxis()->SetTitle("set");
+  gPass->GetYaxis()->SetTitle("pass fraction");
+  gPass->GetYaxis()->SetRangeUser(0.,1.);
+
+  gPass->Draw("ap");
+
+
+  TTree* tEvent=NULL;
+  inFile->GetObject("Event",tEvent); 
+  float evVars[EVVARS];
+  TString evNames[EVVARS];
+  evNames[0]=  TString("run");
+  evNames[1]=  TString("set");
+  evNames[2]=  TString("flag");
+  evNames[3]=  TString("sum");
+  evNames[4]=   TString("singlet");
+  evNames[5]=  TString("triplet");
+  evNames[6]=  TString("late");
+  evNames[7]=  TString("latetime");
+  evNames[8]=  TString("wfsinglet");
+  evNames[9]=  TString("wfmin");
+
+  for(int iv=0; iv< EVVARS; ++iv ) tEvent->SetBranchAddress(evNames[iv],&evVars[iv]);
+
+  printf(" total events %lld \n",tEvent->GetEntries());
+
+  // all entries and fill the histograms 
+  double PPMError[NSETS];
+  int ngood[NSETS];
+  for(int iset=0; iset<NSETS; ++iset) {
+    ngood[iset]=0;
+    PPMError[iset]=0;
+  }
+
+  vector<vector<double>> single;
+  vector<vector<double>> triple;
+  vector<double> tripleRun;
+  vector<double> aveTripleRun;
+  vector<double> errTripleRun;
+  vector<double> singleRun;
+  vector<double> aveSingleRun;
+  vector<double> errSingleRun;
+
+  vector<double> vRun;
+  vector<double> vRunErr;
+  vector<int> vSet;
+
+  single.resize(NSETS);
+  triple.resize(NSETS);
+
+  vector<vector<double>> singleSet;
+  vector<vector<double>> singleSetErr;
+  vector<vector<double>> tripleSet;
+  vector<vector<double>> tripleSetErr;
+
+  singleSet.resize(NSETS);
+  singleSetErr.resize(NSETS);
+  tripleSet.resize(NSETS);
+  tripleSetErr.resize(NSETS);
+
+
+  int thisRun=-1;
+  for (Long64_t jent=0;jent<tEvent->GetEntries() ;jent ++) {
+    tEvent->GetEntry(jent);
+    int iset = evVars[EVSET];
+    int run = evVars[EVRUN] - 20000+30;
+    if(run<0) run = evVars[EVRUN] - 3000;
+    if(run!=thisRun) {
+      if(thisRun>0) {
+        vRun.push_back(double(thisRun));
+        vRunErr.push_back(0.0);
+        vSet.push_back(iset);
+        double ave,err;
+        getStats(tripleRun,ave,err);
+        aveTripleRun.push_back(ave);
+        errTripleRun.push_back(err);
+        tripleSet[iset].push_back(ave);
+        tripleSetErr[iset].push_back(err);
+
+        //printf("run %i size %lu triplet ave %f  err %f   ",thisRun, tripleRun.size(), ave , err );
+        tripleRun.clear();
+        getStats(singleRun,ave,err);
+        aveSingleRun.push_back(ave);
+        errSingleRun.push_back(err);
+        singleSet[iset].push_back(ave);
+        singleSetErr[iset].push_back(err);
+
+        //printf("single  ave %f  err %f  \n ",  ave , err );
+        singleRun.clear();
+      }
+      thisRun=run;
+    }
+    hLate[iset]->Fill(evVars[EVLATE]);
+    hLateTime[iset]->Fill(evVars[EVLATETIME]);
+    bool pass =  -1.0*evVars[EVSINGLET]>singleCut[iset];
+    for(int iv=0; iv< EVVARS; ++iv ) {
+      if(evVars[ EVFLAG]==0&&pass) {
+        ++ngood[iset];
+        single[iset].push_back(evVars[EVSINGLET]);
+        triple[iset].push_back(evVars[EVTRIPLET]);
+        tripleRun.push_back(-1.0*evVars[EVTRIPLET]);
+        singleRun.push_back(-1.0*evVars[EVSINGLET]);
+      }
+    }
+    hSingleSet[iset]->Fill(-1.0*evVars[EVSINGLET]);
+    hTripleSet[iset]->Fill(-1.0*evVars[EVTRIPLET]);
+    hRunTriplet->Fill(run,-1.0*evVars[ETRIPLET]);
+  }
+
+  //for(unsigned j=0; j<vRun.size();++j) printf(" %i %i %f \n",j,int(vRun[j]),aveTripleRun[j]);
+
+  printf(" nruns %lu %lu \n",vRun.size(),aveTripleRun.size());
+
+  double smeanSet[NSETS];
+  double smeanSetErr[NSETS];
+  double tmeanSet[NSETS];
+  double tmeanSetErr[NSETS];
+
+  double setVector[NSETS]={0,1,2,3,4,5};
+  double setVectorErr[NSETS]={0,0,0,0,0,0};
+
+ 
+
+  for(int iset=0 ; iset< NSETS; ++iset) {
+
+    weightedMean( singleSet[iset],singleSetErr[iset], smeanSet[iset],smeanSetErr[iset]);
+    weightedMean( tripleSet[iset],tripleSetErr[iset], tmeanSet[iset],tmeanSetErr[iset]);
+      
+  }
+
+
+
+  TGraphErrors* gSingleSet = new TGraphErrors(NSETS,setVector,smeanSet,setVectorErr,smeanSetErr);
+  TGraphErrors* gTripleSet = new TGraphErrors(NSETS,setVector,tmeanSet,setVectorErr,tmeanSetErr);
+
+ 
+  TGraphErrors* gTripleRun = new TGraphErrors(vRun.size(),&vRun[0],&aveTripleRun[0],&vRunErr[0],&errTripleRun[0]);
+  TCanvas *canRun = new TCanvas("triletRun","tripletRun");
+  gTripleRun->SetName("gTripleRun");
+  gTripleRun->SetTitle("Triple by Run"); 
+  gTripleRun->SetMarkerColor(kRed);
+  gTripleRun->SetMarkerStyle(22);
+  gTripleRun->SetMarkerSize(1.4);
+  gTripleRun->GetXaxis()->SetTitle("run");
+  gTripleRun->GetYaxis()->SetTitle("triplet yield");
+  gTripleRun->Draw("ap");
+  canRun->Print(".png");
+
+
+  TCanvas *canSet = new TCanvas("bySet","bySet");
+  gSingleSet->SetName("gSingleSet");
+  gSingleSet->SetTitle("single by set"); 
+  gSingleSet->SetMarkerColor(kRed);
+  gSingleSet->SetMarkerStyle(22);
+  gSingleSet->SetMarkerSize(1.4);
+  gSingleSet->GetXaxis()->SetTitle("set");
+  gSingleSet->GetYaxis()->SetTitle("yield");
+  gSingleSet->GetYaxis()->SetRangeUser(0,100);
+
+
+  gTripleSet->SetName("gTripleSet");
+  gTripleSet->SetTitle("Triple by set"); 
+  gTripleSet->SetMarkerColor(kBlue);
+  gTripleSet->SetMarkerStyle(23);
+  gTripleSet->SetMarkerSize(1.4);
+  gTripleSet->GetXaxis()->SetTitle("set");
+  gTripleSet->GetYaxis()->SetTitle("yield");
+  gTripleSet->GetYaxis()->SetRangeUser(0,100);
+  gSingleSet->Draw("ap");
+  gTripleSet->Draw("psame");
+  canSet->Print(".png");
+
+
+  
+  TGraphErrors* gSingleRun = new TGraphErrors(vRun.size(),&vRun[0],&aveSingleRun[0],&vRunErr[0],&errSingleRun[0]);
+  TCanvas *canSingletRun = new TCanvas("singleRun","singleRun");
+  gSingleRun->SetName("gSingleRun");
+  gSingleRun->SetTitle("Single by Run"); 
+  gSingleRun->SetMarkerColor(kRed);
+  gSingleRun->SetMarkerStyle(22);
+  gSingleRun->SetMarkerSize(1.4);
+  gSingleRun->GetXaxis()->SetTitle("run");
+  gSingleRun->GetYaxis()->SetTitle("singlet yield");
+  gSingleRun->Draw("ap");
+  canSingletRun->Print(".png");
+
+
+  hSingleSet[5]->SetFillColor(kBlack);
+  hSingleSet[5]->SetFillStyle(3001);
+
+
+
+  TCanvas *canSingleSet = new TCanvas("canSingleSet","canSingleSet");
+  hSingleSet[0]->Draw("");
+  for(int iset=1; iset < NSETS; ++iset) hSingleSet[iset]->Draw("sames");
+  canSingleSet->BuildLegend();
+  canSingleSet->Print(".png");
+
+
+
+  TCanvas *canTripleSet = new TCanvas("canTripleSet","canTripleSet");
+  hTripleSet[0]->Draw("");
+  for(int iset=1; iset < NSETS; ++iset) hTripleSet[iset]->Draw("sames");
+  canTripleSet->BuildLegend();
+  canTripleSet->Print(".png");
+
+
+
+  double singlePeak1[NSETS];
+  double singlePeak2[NSETS];
+
+  for(int iset=0; iset < NSETS; ++iset) {
+    //singlePeak1[iset]=hSingleSet[iset]->GetBinLowEdge(hSingleSet[iset]->GetMaximumBin());
+    double xlow= 0.0;
+    double xhigh=5.0;
+    if(iset==0) {
+      xlow = -1.0;
+      xhigh = 2.0;
+    }
+    singlePeak1[iset]= peakInRange(hSingleSet[iset],xlow,xhigh) ;
+    singlePeak2[iset]= peakInRange(hSingleSet[iset],xhigh,20) ;
+    printf("set %i peak value  %f second %f \n",iset, singlePeak1[iset],singlePeak2[iset]);
+  }
+
+
+
+
+
+  for(int i=0; i<NSETS; ++i) {
+    if(hWave[i]) {
+      hWave[i]->Scale(1./double(ngood[i]));
+      cout << " wave hist " << hWave[i]->GetName() << " nev " << ngood[i] ;
+      cout << " singlet " << hWave[i]->Integral( hWave[i]->FindBin(singletStart), hWave[i]->FindBin(singletEnd)  ) ;
+      cout << " triplet " << hWave[i]->Integral( hWave[i]->FindBin(singletEnd), hWave[i]->FindBin(100000)  ) << endl;
+    }
+  }
+
+
+
+
+  return;
+
+  double smean[NSETS];
+  double srms[NSETS];
+  double tmean[NSETS];
+  double trms[NSETS];
+
+  for(unsigned iset =0; iset<single.size(); ++iset) {
+    getStats(single[iset],smean[iset],srms[iset]);
+    getStats(triple[iset],tmean[iset],trms[iset]);
+
+    printf(" set %u %lu  %f %f %lu %f %f \n",iset, single[iset].size(), smean[iset],srms[iset],triple[iset].size() ,tmean[iset],trms[iset]); 
+  }
+
+
+  //ratios
+  double smeanRatio[NSETS];
+  double srmsRatio[NSETS];
+  double tmeanRatio[NSETS];
+  double trmsRatio[NSETS];
+  for(unsigned iset =0; iset<single.size(); ++iset) {
+    ratioE(smean[iset],srms[iset],smean[0],srms[0],smeanRatio[iset],srmsRatio[iset]);
+    ratioE(tmean[iset],trms[iset],tmean[0],trms[0],tmeanRatio[iset],trmsRatio[iset]);
+  }
+
+
+  TGraphErrors* gSinglet = new TGraphErrors(NSETS,PPM,smeanRatio,PPMError,srmsRatio);
+  gSinglet->SetTitle("good singlet");
+
+  TCanvas *cSinglet = new TCanvas("Singlet","Singlet");
+  cSinglet->SetGridx(); 
+  cSinglet->SetGridy();
+  gSinglet->SetName("SingletGraph");
+  gSinglet->SetTitle("Singlet");
+  gSinglet->SetMarkerColor(kRed);
+  gSinglet->SetMarkerStyle(22);
+  gSinglet->SetMarkerSize(1.4);
+  gSinglet->GetXaxis()->SetTitle("dopant PPM");
+  gSinglet->GetYaxis()->SetTitle("yield");
+  gSinglet->GetYaxis()->SetRangeUser(0,10);
+  gSinglet->Draw("ap");
+  //cSinglet->BuildLegend();
+  cSinglet->Print(".png");
+
+   
+  TGraphErrors* gTriplet = new TGraphErrors(NSETS,PPM,tmeanRatio,PPMError,trmsRatio);
+  gTriplet->SetTitle("good triplet");
+
+  TCanvas *cTriplet = new TCanvas("Triplet","Triplet");
+  cTriplet->SetGridx(); 
+  cTriplet->SetGridy();
+  gTriplet->SetName("TripletGraph");
+  gTriplet->SetTitle("Triplet");
+  gTriplet->SetMarkerColor(kRed);
+  gTriplet->SetMarkerStyle(22);
+  gTriplet->SetMarkerSize(1.4);
+  gTriplet->GetXaxis()->SetTitle("dopant PPM");
+  gTriplet->GetYaxis()->SetTitle("yield");
+  gTriplet->GetYaxis()->SetRangeUser(0,10);
+  gTriplet->Draw("ap");
+  //cTriplet->BuildLegend();
+  cTriplet->Print(".png");
+
+
+
+
+
+  //
+  fout->Write();
+
+}
---
+++ model.C
@@ -0,0 +1,171 @@
+// time is in microseconds
+#include "BaconAnalysis.hh"
+using namespace TMath;
+static double singletFraction = 0.14;
+static double tTriplet=2.100; 
+static double tSinglet=5.E-3;
+static double tXe = 20.0E-3;
+static double kxe = 8.8E-2;
+static double kplusZero  = 1.3E-1;
+static double xstart=1.;
+static double xstop=3.;
+static double tailStart=7.0;
+static double tailStop=10.0;
+static double pmodtG = 3.48;
+double k1Zero = kxe*131./40.;
+double binwidth = 0.04;
+enum {MAXSETS=5};
+double PPM[MAXSETS]={0,1,2,5,10};
+int pcolor[MAXSETS] = {kRed,kMagenta+2,kGreen, kYellow-2,kBlue};
+enum {npars=6};
+TFile *fout;
+double par[6][6];
+
+
+
+
+void printModel(int iset)
+{ 
+  double bw = par[iset][0];
+  double norm = par[iset][1];
+  double kx = k1Zero*par[iset][2];
+  double tau3 = par[iset][3];
+  double kp = par[iset][4];
+  int type = int(par[iset][5]);
+  double td = 1/kx;
+  double tq = 1./(1./tau3+kp);
+  double tr = 1./(kx + 1./tq);
+  double alpha1 = bw*singletFraction*norm;
+  double alpha3 = bw*(1.-singletFraction)*norm;
+
+  printf("set %i tau3 %f alpha1 %E alpha3 %E ratio %E \n",iset,tau3,alpha1,alpha3,alpha3/alpha1);
+}
+
+
+
+static Double_t lightModel(Double_t *xx, Double_t *para)
+{ 
+  double x=xx[0]-xstart;
+  double bw = para[0];
+  double norm = para[1];
+  double kx = k1Zero*para[2];
+  double tau3 = para[3];
+  double kp = para[4];
+  int type = int(para[5]);
+  double td = 1/kx;
+  double tq = 1./(1./tau3+kp);
+  double tr = 1./(kx + 1./tq);
+  double alpha1 = bw*singletFraction*norm;
+  double alpha3 = bw*(1.-singletFraction)*norm;
+  double fs =  alpha1/tSinglet*Exp( -x/tSinglet);
+  double f3 =  alpha3/tau3*Exp(-x/tr);
+  double fx =  alpha3*pow(kx,2)*tq*( Exp( -x/td) - Exp( -x/tr) );
+  double f=0;
+  if (type == 0)
+    f = fs;
+  else if (type == 1)
+    f = f3;
+  else if (type == 2)
+    f = fx;
+  else if (type == 3)
+    f = fs+f3+fx;
+  return f;
+}
+
+
+TF1* setFit(int iset, double norm=1)
+{
+
+  int type = 3;
+  double ppm=PPM[iset]; 
+  double kplus = kplusZero;
+
+  TF1* fp  = new TF1(Form("LfFit-%i-%0.f-PPM", iset, ppm),lightModel,xstart,xstop,npars);
+  fp->SetParName(0,"binw");
+  fp->SetParName(1,"norm");
+  fp->SetParName(2,"PPM");
+  fp->SetParName(3,"tau3");
+  fp->SetParName(4,"k+");
+  fp->SetParName(5,"type");
+  fp->FixParameter(0, par[iset][0]);
+  fp->SetParameter(1, par[iset][1]);
+  fp->SetParameter(2, par[iset][2]);
+  fp->SetParameter(3, par[iset][3]);
+  fp->FixParameter(4, par[iset][4]);
+  fp->FixParameter(5, par[iset][5]);
+  fp->SetTitle(Form("LfFit-%i-%0.f-PPM", iset, ppm));
+  fp->SetNpx(1000); // numb points for function
+  fp->SetLineColor(pcolor[iset]);
+
+  printf(" \n\n >>> setFit starting parameters \n");
+  for (int ii = 0; ii < npars; ++ii)
+  {
+    printf("\t  param %i %s %.3f +/- %.3f \n", ii, fp->GetParName(ii), fp->GetParameter(ii), fp->GetParError(ii));
+  }
+  fout->Append(fp);
+  return fp;
+}
+
+
+
+void model()
+{
+  par[0][0] = 0.005000 ;
+  par[0][1] = 1010853.295568 ;
+  par[0][2] = 0.705848 ;
+  par[0][3] = 1.911888 ;
+  par[0][4] = 0.130000 ;
+  par[0][5] = 3.000000 ;
+  par[1][0] = 0.005000 ;
+  par[1][1] = 2556929.017753 ;
+  par[1][2] = 3.910191 ;
+  par[1][3] = 3.364905 ;
+  par[1][4] = 0.130000 ;
+  par[1][5] = 3.000000 ;
+  par[2][0] = 0.005000 ;
+  par[2][1] = 3075164.849148 ;
+  par[2][2] = 4.902713 ;
+  par[2][3] = 3.876612 ;
+  par[2][4] = 0.130000 ;
+  par[2][5] = 3.000000 ;
+  par[3][0] = 0.005000 ;
+  par[3][1] = 3229307.580083 ;
+  par[3][2] = 7.810860 ;
+  par[3][3] = 4.264110 ;
+  par[3][4] = 0.130000 ;
+  par[3][5] = 3.000000 ;
+  par[4][0] = 0.005000 ;
+  par[4][1] = 3673658.847956 ;
+  par[4][2] = 12.620146 ;
+  par[4][3] = 8.837865 ;
+  par[4][4] = 0.130000 ;
+  par[4][5] = 3.000000 ;
+  par[5][0] = 0.005000 ;
+  par[5][1] = 4764881.683593 ;
+  par[5][2] = 12.600111 ;
+  par[5][3] = 8.188048 ;
+  par[5][4] = 0.130000 ;
+  par[5][5] = 3.000000 ;
+
+  fout = new TFile("model.root","RECREATE");
+  //if(iset>=MAXSETS) return;
+  TF1* fp[MAXSETS];
+  for(int iset=0; iset<MAXSETS; ++iset) {
+    fp[iset] = setFit(iset);
+  }
+
+  TString canTitle;
+  canTitle.Form("lightModel");
+  TCanvas *can = new TCanvas(canTitle,canTitle);
+  fp[MAXSETS-1]->Draw();
+  for(int iset=0; iset<MAXSETS; ++iset) fp[iset]->Draw("same");
+  for(int iset=0; iset<MAXSETS; ++iset) fout->Append(fp[iset]);
+  gPad->SetLogy();
+
+  fout->Write();
+
+  for(int iset=0; iset<MAXSETS; ++iset) {
+    printModel(iset);
+  }
+
+}
